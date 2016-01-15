1ef78bb57e147c65b47643a0339e5d5cce3fdfd5
David Khourshid <davidkpiano@gmail.com>
2016-01-15 15:38:27 -0800

    Exposing actionTypes and adding unit test(s) for actionTypes presence

---
+++ test/action-types-spec.js
@@ -0,0 +1,11 @@
+import { actionTypes } from '../src/index';
+import { assert } from 'chai';
+
+describe('action types', () => {
+  let _actionTypes = Object.keys(actionTypes).map((key) => actionTypes[key]);
+
+  it('should be present and importable from the index file', () => {
+    assert.ok(_actionTypes.length > 0,
+      'there should be at least one action type exported');
+  });
+});
