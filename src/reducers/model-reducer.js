f35058edeb0fa7049c6947a43882581df9287811
David Khourshid <davidkpiano@gmail.com>
2016-02-17 22:25:32 -0500

    Adding immutable model reducers + unit tests

--- src/reducers/model-reducer.js
+++ src/reducers/model-reducer.js
@@ -5,52 +5,61 @@ import toPath from 'lodash/toPath';
 
 import * as actionTypes from '../action-types';
 
-function createModelReducer(model, initialState = {}) {
-  const modelPath = toPath(model);
-
-  return (state = initialState, action) => {
-    if (!action.model) return state;
-
-    const path = toPath(action.model);
-
-    if (!isEqual(path.slice(0, modelPath.length), modelPath)) {
-      return state;
-    }
-
-    const localPath = path.slice(modelPath.length);
-
-    switch (action.type) {
-      case actionTypes.CHANGE:
-        if (!localPath.length) {
-          return action.value;
-        }
-
-        if (isEqual(get(state, localPath), action.value)) {
-          return state;
-        }
-
-        return icepick.setIn(state, localPath, action.value);
-
-      case actionTypes.RESET:
-        if (!localPath.length) {
-          return initialState;
-        }
-
-        if (isEqual(get(state, localPath), get(initialState, localPath))) {
-          return state;
-        }
-
-        return icepick.setIn(
-          state,
-          localPath,
-          get(initialState, localPath));
-
-      default:
-        return state;
-    }
-  };
+function icepickSet(state, path, value) {
+  return icepick.setIn(state, path, value);
 }
 
+function createModeler(getter = get, setter = icepickSet, initialModelState = {}) {
+  return function createModelReducer(model, initialState = initialModelState) {
+    const modelPath = toPath(model);
+
+    return (state = initialState, action) => {
+      if (!action.model) return state;
+
+      const path = toPath(action.model);
+
+      if (!isEqual(path.slice(0, modelPath.length), modelPath)) {
+        return state;
+      }
+
+      const localPath = path.slice(modelPath.length);
+
+      switch (action.type) {
+        case actionTypes.CHANGE:
+          if (!localPath.length) {
+            return action.value;
+          }
+
+          if (isEqual(getter(state, localPath), action.value)) {
+            return state;
+          }
+
+          return setter(state, localPath, action.value);
+
+        case actionTypes.RESET:
+          if (!localPath.length) {
+            return initialState;
+          }
+
+          if (isEqual(getter(state, localPath), getter(initialState, localPath))) {
+            return state;
+          }
+
+          return setter(
+            state,
+            localPath,
+            getter(initialState, localPath));
+
+        default:
+          return state;
+      }
+    }
+  }
+}
+
+const createModelReducer = createModeler();
+
 export {
+  createModeler,
   createModelReducer
 };
