423b41dfd68a0986d11b60823ff77dc2d3484805
chrisblossom <cbloss@gmail.com>
2016-05-17 08:15:50 -0700

    added all peerDeps to webpack externals and added missing webpack-bundle-size-analyzer to devDeps

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -9,23 +9,35 @@ module.exports = {
       commonjs: 'react',
       amd: 'react',
     },
-    redux: {
-      root: 'Redux',
-      commonjs2: 'redux',
-      commonjs: 'redux',
-      amd: 'redux',
-    },
     'react-addons-shallow-compare': {
       root: 'react-addons-shallow-compare',
       commonjs2: 'react-addons-shallow-compare',
       commonjs: 'react-addons-shallow-compare',
       amd: 'react-addons-shallow-compare',
     },
+    'react-dom': {
+      root: 'react-dom',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
     'react-redux': {
       root: 'ReactRedux',
       commonjs2: 'react-redux',
       commonjs: 'react-redux',
       amd: 'react-redux',
+    },
+    redux: {
+      root: 'Redux',
+      commonjs2: 'redux',
+      commonjs: 'redux',
+      amd: 'redux',
+    },
+    'redux-thunk': {
+      root: 'redux-thunk',
+      commonjs2: 'redux-thunk',
+      commonjs: 'redux-thunk',
+      amd: 'redux-thunk',
     },
   },
   module: {
