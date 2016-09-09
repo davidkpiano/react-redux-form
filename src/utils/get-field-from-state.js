da4b580550ef1a2cd743608427045c1266c44d10
David Khourshid <davidkpiano@gmail.com>
2016-09-08 23:58:23 -0400

    Preventing issue with "in" being used on non-objects

--- src/utils/get-field-from-state.js
+++ src/utils/get-field-from-state.js
@@ -2,6 +2,7 @@ import get from './get';
 import toPath from './to-path';
 import { initialFieldState } from '../reducers/form-reducer';
 import getForm from './get-form';
+import isPlainObject from 'lodash/isPlainObject';
 
 export default function getFieldFromState(state, modelString) {
   const form = getForm(state, modelString);
@@ -12,10 +13,9 @@ export default function getFieldFromState(state, modelString) {
 
   const formPath = toPath(form.$form.model);
   const fieldPath = toPath(modelString).slice(formPath.length);
-
   const field = get(form, fieldPath, initialFieldState);
 
-  if (field && '$form' in field) return field.$form;
+  if (isPlainObject(field) && '$form' in field) return field.$form;
 
   if (!field) return initialFieldState;
 
