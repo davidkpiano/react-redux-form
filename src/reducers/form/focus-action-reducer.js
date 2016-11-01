bed41c18204867c8fc45cffbdc0c768bcfa83107
David Khourshid <davidkpiano@gmail.com>
2016-11-01 14:04:33 -0400

    Reducing code, WIP

--- src/reducers/form/focus-action-reducer.js
+++ src/reducers/form/focus-action-reducer.js
@@ -1,17 +1,51 @@
 import actionTypes from '../../action-types';
-import updateField from '../../utils/update-field';
+import updateField, { getFieldAndForm } from '../../utils/update-field';
+import getFieldForm from '../../utils/get-field-form';
 
 export default function setFocusActionReducer(state, action, localPath) {
-  if (action.type !== actionTypes.FOCUS) {
-    return state;
+  const [field] = getFieldAndForm(state, localPath);
+
+  const updatedField = {};
+
+  switch (action.type) {
+    case actionTypes.FOCUS:
+      Object.assign(updatedField, {
+        focus: true,
+        intents: action.silent
+          ? []
+          : [action],
+      });
+
+      break;
+
+    case actionTypes.BLUR:
+    case actionTypes.SET_TOUCHED: {
+      const fieldForm = getFieldForm(state, localPath).$form;
+
+      Object.assign(updatedField, {
+        focus: action.type === actionTypes.BLUR
+          ? false
+          : field.focus,
+        touched: true,
+        retouched: fieldForm
+          ? !!(fieldForm.submitted || fieldForm.submitFailed)
+          : false,
+      });
+
+      break;
+    }
+
+    case actionTypes.SET_UNTOUCHED:
+      Object.assign(updateField, {
+        focus: false,
+        touched: false,
+      });
+
+      break;
+
+    default:
+      return state;
   }
 
-  if (action.silent) {
-    return updateField(state, localPath, { focus: true });
-  }
-
-  return updateField(state, localPath, {
-    focus: true,
-    intents: [action],
-  });
+  return updateField(state, localPath, updatedField);
 }
