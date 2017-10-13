b62e393ac03bab02bee5492bcef840f266ec7927
David Khourshid <davidkpiano@gmail.com>
2017-10-13 13:13:44 -0400

    Removing immutable dependency from find-key-immutable. Addresses #969

--- src/utils/find-key-immutable.js
+++ src/utils/find-key-immutable.js
@@ -1,22 +1,3 @@
-import { List } from 'immutable';
-
 export default function findKey(object, predicate) {
-  let resultKey;
-
-  if (List.isList(object)) {
-    resultKey = object.findKey(predicate);
-  } else {
-    Object.keys(object).some((key) => {
-      const isKey = predicate(object[key], key, object);
-
-      if (isKey) {
-        resultKey = key;
-        return true;
-      }
-
-      return false;
-    });
-  }
-
-  return resultKey;
+  return object.findKey(predicate);
 }
