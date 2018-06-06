821e93be31da90133e2f93df7612c1550a429a2b
Mitchell Demler <mitchell.demler@harcourts.net>
2018-06-06 15:53:49 +1200

    Revert defaulting control value to a string
    This reverts the change in #1151. Control should not determine what type the value should be. Control needs to return the value as is.

--- src/components/control-component-factory.js
+++ src/components/control-component-factory.js
@@ -664,10 +664,6 @@ function createControlClass(s) {
           controlProps.children
         );
       }
-      // Set a default value to prevent it being treated as uncontrolled input
-      if (!controlProps.value && !mappedProps.value) {
-        mappedProps.value = '';
-      }
       return createElement(
         ComponentWrapper,
         {
